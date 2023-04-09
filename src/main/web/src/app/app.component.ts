68380848c32e506d61966f2d95f33cb715e66985
Ask Bisgaard <ask@yggdrasil.solutions>
2023-04-09 11:51:07 +0200

    Layout

--- src/main/web/src/app/app.component.ts
+++ src/main/web/src/app/app.component.ts
@@ -1,10 +1,34 @@
 import { Component } from '@angular/core';
+import { scheduled_pickup } from './scheduled-pickup';
+import {NgbDate, NgbDateAdapter, NgbDateStruct, NgbCalendar} from '@ng-bootstrap/ng-bootstrap';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
+
 export class AppComponent {
   title = 'customerplatform';
+
+  
+  new_pickup: scheduled_pickup = {
+    id: -1,
+    pickup_date: new NgbDate(new Date(Date.now()).getFullYear(), new Date(Date.now()).getMonth(), new Date(Date.now()).getDay()),
+    weight: 0
+  };
+
+  scheduled_pickups:scheduled_pickup[] =[
+    { id: 1, pickup_date: new NgbDate(2023, 4, 1), weight: 50 },
+    { id: 2, pickup_date: new NgbDate(2023, 5, 1), weight: 23 }
+  ];
+
+  schedulePickup(schedule_pickup: scheduled_pickup): void {
+    console.log('Pickup: %dkg on %s', schedule_pickup.weight, schedule_pickup.pickup_date)
+    debugger;
+  };
+
+  cancelPickup(scheduled_pickup: scheduled_pickup): void {
+    console.log('Remove pickup: %d', scheduled_pickup.id)
+  };
 }
---
+++ src/main/web/src/app/scheduled-pickup.ts
@@ -0,0 +1,7 @@
+import {NgbDate} from '@ng-bootstrap/ng-bootstrap';
+
+export interface scheduled_pickup {
+    id: number;
+    pickup_date: NgbDate;
+    weight: number;
+}
